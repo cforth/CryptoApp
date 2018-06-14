81ebb03715ed8024217c0bca3b745e48c1cffaf9
cforth <cforth@163.com>
2018-06-14 12:53:29 +0800

    增加加密的状态提示

--- TextLook.py
+++ TextLook.py
@@ -1,8 +1,11 @@
 import os
+import logging
 import tkinter.filedialog as filedialog
+import tkinter.messagebox as tkmessagebox
 from libs.json2gui import *
 from libs.CFCrypto import StringCrypto
 
+logging.basicConfig(level=logging.ERROR)
 
 # 窗口类
 class Window(ttk.Frame):
@@ -30,7 +33,7 @@ class Window(ttk.Frame):
         self.file_save(self.current_file_path)
 
     def file_save_new_button_callback(self, event=None):
-        new_file_path = filedialog.asksaveasfilename()
+        new_file_path = os.path.abspath(filedialog.asksaveasfilename())
         self.file_save(new_file_path)
         self.current_file_path = new_file_path
         self.__dict__["filePath"].set(self.current_file_path)
@@ -43,10 +46,21 @@ class Window(ttk.Frame):
         self.__dict__["fileShowText"].delete(0.0, 'end')
         with open(self.current_file_path, "r") as f:
             self.file_text = f.read()
-
+            self.__dict__["fileSaveStatus"].set("")
             if crypto_option == "输入密码":
                 password = self.__dict__["password"].get()
-                self.file_text = StringCrypto(password).decrypt(self.file_text)
+                try:
+                    self.file_text = StringCrypto(password).decrypt(self.file_text)
+                    self.__dict__["fileCryptoStatus"].set("[已加密]")
+                except Exception as e:
+                    self.file_text = ""
+                    self.__dict__["filePath"].set("")
+                    self.__dict__["fileCryptoStatus"].set("")
+                    self.__dict__["fileSaveStatus"].set("")
+                    logging.error("Text Decrypt Error！！！")
+                    tkmessagebox.showerror("错误", "文本格式或密码错误！")
+            else:
+                self.__dict__["fileCryptoStatus"].set("")
 
             self.__dict__["fileShowText"].insert('end', self.file_text)
 
@@ -57,9 +71,13 @@ class Window(ttk.Frame):
         if crypto_option == "输入密码":
             password = self.__dict__["password"].get()
             save_text = StringCrypto(password).encrypt(save_text)
+            self.__dict__["fileCryptoStatus"].set("[已加密]")
+        else:
+            self.__dict__["fileCryptoStatus"].set("")
 
         with open(file_path, "w") as f:
             f.write(save_text)
+            self.__dict__["fileSaveStatus"].set("[已保存]")
 
 
 if __name__ == '__main__':
