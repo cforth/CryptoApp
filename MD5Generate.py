08c4aa450b3cfffc24e7ab1d5e2601ffd8b0806d
cforth <cforth@163.com>
2018-06-29 10:06:51 +0800

    文件夹MD5值功能

--- MD5Generate.py
+++ MD5Generate.py
@@ -13,10 +13,16 @@ class Window(ttk.Frame):
         create_ui(self, ui_json)
         # 从json自动绑定事件
         create_all_binds(self, ui_json)
+        # 设置滚动条
+        set_scrollbar(self.__dict__["MD5ResultText"],
+                      self.__dict__["TextScrollbarX"],
+                      self.__dict__["TextScrollbarY"])
+        # 设置默认的选项
+        set_combobox_item(self.__dict__["fileFromOptionCombobox"], "文件", True)
         self.master.columnconfigure(0, weight=1)
         self.master.rowconfigure(0, weight=1)
         self.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.E, tk.W))
-        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
         self.rowconfigure(1, weight=1)
 
     # 禁用按钮
@@ -29,26 +35,45 @@ class Window(ttk.Frame):
         self.__dict__["MD5GenerateButton"]["state"] = "normal"
         self.__dict__["MD5GenerateButton"]["text"] = "MD5生成"
 
-    def md5_generate(self, file_name):
-        result_text = get_file_md5(file_name)
+    def show_md5_info(self, file_name, result_text):
         self.__dict__["MD5ResultText"].insert('end', "文件路径: " + file_name)
         self.__dict__["MD5ResultText"].insert('end', "\n")
         self.__dict__["MD5ResultText"].insert('end', "MD5值: ")
         self.__dict__["MD5ResultText"].insert('end', result_text)
         self.__dict__["MD5ResultText"].insert('end', "\n\n")
+
+    def file_md5_generate(self, file_path):
+        self.disable_button()
+        result_text = get_file_md5(file_path)
+        self.show_md5_info(file_path, result_text)
         self.allow_button()
 
+    def dir_md5_generate(self, dir_path):
+        for path, subdir, files in os.walk(dir_path):
+            for f in files:
+                file_path = os.path.join(os.path.abspath(path), f)
+                self.file_md5_generate(file_path)
+
     def file_from_button_callback(self, event=None):
-        self.__dict__["file_from_path"].set(filedialog.askopenfilename())
+        if self.__dict__["fileFromOption"].get() == "文件":
+            self.__dict__["file_from_path"].set(filedialog.askopenfilename())
+        elif self.__dict__["fileFromOption"].get() == "文件夹":
+            self.__dict__["file_from_path"].set(filedialog.askdirectory())
 
     def md5_generate_button_callback(self, event=None):
         file_name = self.__dict__["file_from_path"].get()
-        if not os.path.isfile(file_name):
-            return
-        else:
-            self.disable_button()
-            md5_gen_thread = Thread(target=self.md5_generate, args=(file_name,))
-            md5_gen_thread.start()
+        if self.__dict__["fileFromOption"].get() == "文件":
+            if not os.path.isfile(file_name):
+                return
+            else:
+                md5_gen_thread = Thread(target=self.file_md5_generate, args=(file_name,))
+                md5_gen_thread.start()
+        elif self.__dict__["fileFromOption"].get() == "文件夹":
+            if not os.path.isdir(file_name):
+                return
+            else:
+                md5_gen_thread = Thread(target=self.dir_md5_generate, args=(file_name,))
+                md5_gen_thread.start()
 
 
 if __name__ == '__main__':
