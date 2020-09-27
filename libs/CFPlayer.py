51c5a6f243f0f1c967aa62c0c905a6a810fde1d9
chaifei <cforth@163.com>
2020-09-27 12:39:12 +0800

    空格暂停

--- libs/CFPlayer.py
+++ libs/CFPlayer.py
@@ -10,15 +10,30 @@ def play(video_path):
     try:
         media_load = pyglet.media.load(video_path)
         v_format = media_load.video_format
-        window = pyglet.window.Window(width=v_format.width, height=v_format.height)
+        window = pyglet.window.Window(width=v_format.width, height=v_format.height,
+                                      caption="VideoPlayer", resizable=True)
         player.queue(media_load)
         player.play()
 
         @window.event
         def on_draw():
             window.clear()
+            x_pos = (window.width - v_format.width) / 2
+            y_pos = (window.height - v_format.height) / 2
+            x_pos = x_pos if x_pos >= 0 else 0
+            y_pos = y_pos if y_pos >= 0 else 0
+
             if player.source and player.source.video_format:
-                player.get_texture().blit(0, 0)
+                player.get_texture().blit(x_pos, y_pos)
+
+        @window.event
+        def on_key_press(symbol, modifiers):
+            # 空格暂停
+            if symbol == 32:
+                if player.playing:
+                    player.pause()
+                else:
+                    player.play()
 
     except Exception as e:
         print('CFPlayer Error:', e)
